4bb555852dc9e4c9b81adff46434715d2edd3cb3
cherry <515811019@qq.com>
2019-06-14 18:47:00 +0800

    rabbitMQ初窥

---
+++ src/test/java/com/cherry/rabbitmqstudy/RabbitmqstudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cherry.rabbitmqstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqstudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
