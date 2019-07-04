abf0a0e7005bcf4cca58a3430f08a3cfc918f9bb
cherry <515811019@qq.com>
2019-07-04 20:31:13 +0800

    message相关API

--- src/main/java/com/cherry/rabbitmqstudy/message/Consumer.java
+++ src/main/java/com/cherry/rabbitmqstudy/message/Consumer.java
@@ -1,27 +1,17 @@
-package com.cherry.rabbitmqstudy.quickstart;
+package com.cherry.rabbitmqstudy.message;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
 
+import java.util.Map;
+
 /**
  * @Auther: cherry
- * @Date: 2019-06-13 22:00
+ * @Date: 2019-06-26 15:15
  */
 public class Consumer {
-
-    /**
-     * 消费者需要从队列中获取消息进行消费
-     *
-     * 1.创建连接工厂ConnectionFactory
-     * 2.获取rabbitMQ的连接connection
-     * 3.通过连接开辟通道channel
-     * 4.通过channel声明队列queue
-     * 5.创建消费者进行消费
-     * 6.设置消费者需要监听的队列，以及ACK模式
-     * 7.消费
-     */
     public static void main(String[] args) throws Exception {
         //1.创建连接工厂，并配置属性
         ConnectionFactory connectionFactory = new ConnectionFactory();
@@ -50,7 +40,8 @@ public class Consumer {
         while(true) {
             QueueingConsumer.Delivery delivery = queueingConsumer.nextDelivery();
             String msg = new String(delivery.getBody());
-            System.out.println("消费者端：" + msg);
+            Map<String,Object> header = delivery.getProperties().getHeaders();
+            System.out.println("消费者端：" + msg + ",get the header: " + header.toString());
         }
     }
 }
--- src/main/java/com/cherry/rabbitmqstudy/message/Producer.java
+++ src/main/java/com/cherry/rabbitmqstudy/message/Producer.java
@@ -1,23 +1,23 @@
-package com.cherry.rabbitmqstudy.quickstart;
+package com.cherry.rabbitmqstudy.message;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Auther: cherry
- * @Date: 2019-06-13 21:59
+ * @Date: 2019-06-26 15:15
  */
 public class Producer {
-    /**
-     * 生产者生产消息并发送给对应的交换机
-     *
-     * 1.创建连接工厂ConnectionFactory
-     * 2.获取rabbitMQ的连接connection
-     * 3.通过连接开辟通道channel
-     * 4.通过channel发送数据
-     * 5.释放资源
+
+    /*
+    发送带有附加属性的消息
      */
+
     public static void main(String[] args) throws Exception {
         //1.创建连接工厂，并配置属性
         ConnectionFactory connectionFactory = new ConnectionFactory();
@@ -33,18 +33,28 @@ public class Producer {
         //3.通过连接开辟通道
         Channel channel = connection.createChannel();
 
+        Map<String,Object> header = new HashMap<>();
+        header.put("myValue1","001");
+        header.put("myValue2","002");
+        header.put("myValue3","003");
+
+
+        //创建properties
+        AMQP.BasicProperties props = new AMQP.BasicProperties().builder()
+                .deliveryMode(1) //设置消息投递的模式
+                .contentEncoding("UTF-8")
+                .expiration("5000")
+                .headers(header)
+                .build();
+
         //4.通过channel发送数据
         for (int i = 0; i < 5; i++) {
             String msg = i + " hello rabbitMQ!";
-            //注意由于此时没有指定交换机，那么采用默认的交换机即AMQP-default：
-            //The default exchange is implicitly bound to every queue, with a routing key equal to the queue name.
-            //只要routingKey与队列的名字相同那么该默认的交换机就与该队列绑定，因此消息即可传递到该队列中
-            channel.basicPublish("","cherry001",null,msg.getBytes());
+            channel.basicPublish("","cherry001",props,msg.getBytes());
         }
 
         //5.释放资源
         channel.close();
         connection.close();
-
     }
 }
